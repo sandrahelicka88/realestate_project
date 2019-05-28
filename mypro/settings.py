459330d3e5e3a53b569bf91c0703e0860001f0d0
Sandra Helicka <sand88ra.h@gmail.com>
2019-05-28 01:12:08 +0100

    Contact inquiries

--- mypro/settings.py
+++ mypro/settings.py
@@ -34,6 +34,8 @@ INSTALLED_APPS = [
     'pages.apps.PagesConfig',
     'listings.apps.ListingsConfig',
     'realtors.apps.RealtorsConfig',
+    'accounts.apps.AccountsConfig',
+    'contacts.apps.ContactsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -134,3 +136,16 @@ STATICFILES_DIRS = [
 #Media folder settings
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
+
+#Messages
+from django.contrib.messages import constants as messages
+MESSAGE_TAGS = {
+    messages.ERROR: 'danger',
+    50: 'critical',
+}
+#Email config 
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT =587
+EMAIL_HOST_USER = 'sand88ra.h@gmail.com'
+EMAIL_HOST_PASSWORD = 'Paracelsus2262'
+EMAIL_USE_TLS = True
