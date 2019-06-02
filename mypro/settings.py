62c55edd9e1abb3d14d99674b6cabcafcf5aad41
SandraHelicka <helicka_s@interia.pl>
2019-06-02 03:14:03 +0100

    Last changes in settings

--- mypro/settings.py
+++ mypro/settings.py
@@ -147,5 +147,5 @@ MESSAGE_TAGS = {
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT =587
 EMAIL_HOST_USER = 'sand88ra.h@gmail.com'
-EMAIL_HOST_PASSWORD = 'Paracelsus2262'
+EMAIL_HOST_PASSWORD = '***'
 EMAIL_USE_TLS = True
